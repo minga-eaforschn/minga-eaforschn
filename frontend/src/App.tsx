c235f0d5149a01a74fe7d9d746006768cb813911
Julian Hartl <julian.hartl04@gmail.com>
2022-11-19 13:28:08 +0100

    Frontend: Add Theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Router from './router/router';
 import CssBaseline from '@mui/material/CssBaseline';
+import {ThemeProvider} from "@mui/material";
+import {theme} from "./theme";
 
 function App() {
   return (<React.Fragment>
     <CssBaseline/>
-    <Router/>
+    <ThemeProvider theme={theme}> <Router/>
+    </ThemeProvider>
   </React.Fragment>)
 }
 
