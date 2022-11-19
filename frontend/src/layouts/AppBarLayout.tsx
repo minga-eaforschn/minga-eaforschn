7bcbe07e1cec050f88925fc4412e9c31841805f5
Florian Glombik <florian.glombik@selectcode.de>
2022-11-19 14:47:19 +0100

    Changing header label

--- frontend/src/layouts/AppBarLayout.tsx
+++ frontend/src/layouts/AppBarLayout.tsx
@@ -1,22 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import {NavLink as RouterLink, Outlet,} from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { NavLink as RouterLink, Outlet } from "react-router-dom";
 
 export default function AppBarLayout() {
-  return <Box>
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-          React App
-        </Typography>
-        <Button color="inherit"  component={RouterLink} to={'/home'}>Home</Button>
-        <Button color="inherit" component={RouterLink} to={'/page1'}>Page 1</Button>
-      </Toolbar>
-    </AppBar>
-    <Outlet/>
-  </Box>
+  return (
+    <Box>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Minga eaforschn
+          </Typography>
+          <Button color="inherit" component={RouterLink} to={"/home"}>
+            Home
+          </Button>
+          <Button color="inherit" component={RouterLink} to={"/page1"}>
+            Page 1
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </Box>
+  );
 }
